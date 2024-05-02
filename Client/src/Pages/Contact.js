bc3b4dce9055cb7d090edc5e9847d99bf168ef01
Dinukanth <pdinukanth@gmail.com>
2024-05-02 13:58:33 +0530

    UI-Design

---
+++ Client/src/Pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Contact from "../Components/Contact/Contact";
+
+function contactform() {
+    return(
+        <div>
+            <Contact/>
+
+        </div>
+    )
+    
+}
+export default contactform()
---
+++ Client/src/Pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import './Home.css'
+import Navbar from "../Components/Navbar/Navbar";
+import Landpage from "../Components/Landpage/Landpage";
+
+function Home() {
+
+    return(
+        <div>
+
+            <Navbar/>
+            <Landpage/>
+
+           
+
+        </div>
+    )
+    
+}
+
+export default Home()
